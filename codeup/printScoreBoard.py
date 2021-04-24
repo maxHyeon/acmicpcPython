0c4e7db1a0c7f513ade6e97cb028d848d7f8dd3d
minhyeon <parkmh90@kr.ibm.com>
2021-04-24 14:41:00 +0900

    solve 3015

---
+++ codeup/printScoreBoard.py
@@ -0,0 +1,22 @@
+class Score:
+    def __init__(self):
+        self.lineAmount = 0
+        self.printAmount = 0
+        self.scores = []
+
+def procInputs(dataSize):
+    metaData = list(map(int,dataSize.split()))
+    score = Score()
+    score.lineAmount = metaData[0]
+    score.printAmount = metaData[1]
+    for i in range(list(map(int,dataSize.split())).pop(0)):
+        score.scores.append(list(map(lambda x: int(x) if x.isdigit() else x, input().split())))
+    return score
+
+def printScoreBoard(inputs):
+    sortedScore = sorted(inputs.scores,key=lambda input: (input[1],input.index(input[0])),reverse=True)
+    for i in range(inputs.printAmount):
+        print(sortedScore[i][0])
+
+if __name__ == "__main__":
+    printScoreBoard(procInputs(input()))
